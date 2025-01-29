c3d14c5b59a74c3d5498a5579f1f340ff6c8ac42
Isaac Murillo <isaacmurillo07@gmail.com>
2025-01-28 20:29:45 -0600

    created and added navbar which includes the logo and four buttons that can redirect to different pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,11 +9,12 @@ import Cart from './pages/Cart'
 import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vm] md:px-[7vm] lg:px-[9vm]'>
-
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/collection' element={<Collection/>} />
@@ -24,7 +25,6 @@ const App = () => {
         <Route path='/login' element={<Login/>} />
         <Route path='/place-order' element={<PlaceOrder/>} />
         <Route path='/orders' element={<Orders/>} />
-
       </Routes>
       
     </div>
